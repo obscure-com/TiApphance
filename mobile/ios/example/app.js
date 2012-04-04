529c0a134ff818918b701e9f5149d87609ce39ad
Paul Mietz Egli <paul@obscure.com>
2012-04-04 09:38:09 -0700

    added logging example

--- mobile/ios/example/app.js
+++ mobile/ios/example/app.js
@@ -10,5 +10,8 @@ window.open();
 var tiapphance = require('com.obscure.TiApphance');
 Ti.API.info("module is => " + tiapphance);
 
-tiapphance.startNewSession("YOUR_APPHANCE_API_KEY_HERE", tiapphance.MODE_QA);
+tiapphance.startNewSession("a3f78c237a2f540892dfff409ec83a60952518a8", tiapphance.MODE_QA);
 label.text = 'started session';
+
+tiapphance.APHExtendedLog(tiapphance.LEVEL_INFO, 'sometag', String.format('the time is %s', (new Date()).toString()));
+label.text = 'logged';
