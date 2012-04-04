b714377569e88fdaa19cfc3bf95488b668848681
Paul Mietz Egli <paul@obscure.com>
2012-04-04 10:35:40 -0700

    removed Apphance key from example

--- mobile/ios/example/app.js
+++ mobile/ios/example/app.js
@@ -10,7 +10,7 @@ window.open();
 var tiapphance = require('com.obscure.TiApphance');
 Ti.API.info("module is => " + tiapphance);
 
-tiapphance.startNewSession("a3f78c237a2f540892dfff409ec83a60952518a8", tiapphance.MODE_QA);
+tiapphance.startNewSession("YOUR_APPHANCE_KEY_HERE", tiapphance.MODE_QA);
 label.text = 'started session';
 
 tiapphance.APHExtendedLog(tiapphance.LEVEL_INFO, 'sometag', String.format('the time is %s', (new Date()).toString()));
